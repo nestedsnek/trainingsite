2866e661b69d3627d2d97b7706dfeedfc9e6121d
anaisjin <anaisjingit@gmail.com>
2026-01-16 22:59:11 +0100

    Create textnode class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    node1 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node1)
+
+
+if __name__ == "__main__":
+    main()
